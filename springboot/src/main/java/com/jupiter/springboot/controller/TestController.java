4abdd9ad62f8cf69c32ccc1d229ab914f9b7d81a
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-05-09 10:46:47 +0900

    Connection test (#12)
    * 0509_connection_test
    
    * 0509_connection_test
    0509_springboot_version_change(3.0.6 -> 2.7.11)

--- springboot/src/main/java/com/jupiter/springboot/controller/TestController.java
+++ springboot/src/main/java/com/jupiter/springboot/controller/TestController.java
@@ -1,10 +1,10 @@
 package com.jupiter.springboot.controller;
 
+import com.jupiter.springboot.domain.Message;
 import com.jupiter.springboot.domain.Test;
-import com.jupiter.springboot.domain.Test2;
-import com.jupiter.springboot.persistence.Test2Repository;
-import com.jupiter.springboot.persistence.TestRepository;
+import com.jupiter.springboot.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,41 +13,35 @@ import java.util.List;
 @RestController
 public class TestController {
 
-
     @Autowired
-    TestRepository testRepo;
+    TestService testService;
 
-    @Autowired
-    Test2Repository test2Repo;
+    @GetMapping("/api/test")
+    public ResponseEntity<?> test() {
 
-    @ResponseBody
-    @GetMapping("/api/send/{userName}")
-    public String getMessage(@PathVariable String userName) {
-        System.out.println(userName);
-        return userName;
+        System.out.println("controller 진입지점");
+        
+        return testService.test();
+    }
+
+    @GetMapping("/api/getData")
+    public List<Test> getData() {
+
+        System.out.println("controller : 리액트로 데이터 보내주기");
+
+        return testService.getData();
     }
 
     @ResponseBody
-    @PostMapping("/api/send") //
-    public Test getMessage(@RequestBody Test params) {
-        System.out.println(params.getUserName());
-        System.out.println(params.getPassword());
+    @PostMapping("/api/sendData")
+    public Message getMessage(@RequestBody Message params) {
+
+        System.out.println("controller : 리액트에서 데이터 받아오기");
+        System.out.println(params.getDate());
+        System.out.println(params.getTime());
 
         return params;
     }
 
-    @ResponseBody
-    @PostMapping("/api/send1") //
-    public void insertMessage(@RequestBody Test params) {
-        System.out.println(params.getUserName());
-        System.out.println(params.getPassword());
 
-        testRepo.save(params);
-    }
-
-    @GetMapping("/api/getData")
-    public List<Test2> getData() {
-
-        return (List<Test2>) test2Repo.findAll();
-    }
 }
--- springboot/src/main/java/com/jupiter/springboot/persistence/TestRepository.java
+++ springboot/src/main/java/com/jupiter/springboot/persistence/TestRepository.java
@@ -1,7 +1,8 @@
 package com.jupiter.springboot.persistence;
 
 import com.jupiter.springboot.domain.Test;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TestRepository extends CrudRepository<Test, Long> {
+public interface TestRepository extends JpaRepository<Test, Long> {
+
 }
