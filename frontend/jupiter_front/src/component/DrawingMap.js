ca67c5580c54bad19daec3341b05cf7e32d188c4
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-05-18 17:26:32 +0900

    0518 프론트엔트작업 (#2)
    * 마커 클러스터링
    
    * 클러스터 찍으면 인덱스 반환하기

--- frontend/jupiter_front/src/component/DrawingMap.js
+++ frontend/jupiter_front/src/component/DrawingMap.js
@@ -13,7 +13,7 @@ const DrawingMap = () => {
     // console.log(suddenAcc)
     // console.log(suddenDrop)
     // console.log(suddenDeparture)
-    console.log(suddenStop)
+    // console.log(suddenStop)
 
     useEffect(() => {
 
@@ -43,42 +43,81 @@ const DrawingMap = () => {
         let SuddenStopmarker = new kakao.maps.MarkerImage(imgSuddenStop, imageSize);
 
         // 마커를 생성
-        let Accmarker = suddenAcc.map((i) =>
-            new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+        let AccIdx = 0;
+        let Accmarkers = suddenAcc.map((i) => {
+          let Accmarker = new kakao.maps.Marker({
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급가속", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenAccmarker // 마커 이미지 
             })
+            Accmarker.idx = 'Accmarker' + ++AccIdx;
+            return Accmarker;
+        }
         );
 
-        let Dropmarker = suddenDrop.map((i) =>
-            new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+        let DropIdx = 0;
+        let Dropmarkers = suddenDrop.map((i) => {
+            let Dropmarker = new kakao.maps.Marker({
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급감속", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenDropmarker // 마커 이미지 
             })
+            Dropmarker.idx = 'Dropmarker' + ++DropIdx;
+            return Dropmarker;
+        }
         );
 
-        let Depmarker = suddenDeparture.map((i) =>
-            new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+        let DepIdx = 0;
+        let Depmarkers = suddenDeparture.map((i) => {
+            let Depmarker = new kakao.maps.Marker({
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급출발", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenDeparturemarker // 마커 이미지 
             })
+            Depmarker.idx = 'Depmarker' + ++DepIdx;
+            return Depmarker;
+        }
         );
 
-        let Stopmarker = suddenStop.map((i) =>
-            new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
+        let StopIdx = 0;
+        let Stopmarkers = suddenStop.map((i) => {
+            
+            let Stopmarker = new kakao.maps.Marker({
+                // map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(i.GPS_Y, i.GPS_X), // 마커를 표시할 위치
                 title: "급정지", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: SuddenStopmarker // 마커 이미지 
             })
-        );
+            Stopmarker.idx = ++StopIdx;
 
+            return Stopmarker;
+
+        });
+
+        // console.log(Stopmarkers)
+
+        // 마커 클러스터러 생성
+        let clusterer = new kakao.maps.MarkerClusterer({
+            map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+            averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+            minLevel: 5, // 클러스터 할 최소 지도 레벨 
+            disableClickZoom: true
+        })
+
+        clusterer.addMarkers(Accmarkers)
+        clusterer.addMarkers(Dropmarkers)
+        clusterer.addMarkers(Depmarkers)
+        clusterer.addMarkers(Stopmarkers)
+
+        // console.log('Stopmarker: ', Stopmarkers[3].idx)
+        // 마커 클러스터러에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(clusterer, 'clusterclick', function(cluster) {
+            console.log(cluster.getMarkers());
+            cluster.getMarkers().forEach(marker => console.log(marker.idx));
+        });
 
 
     }, []);
@@ -87,7 +126,7 @@ const DrawingMap = () => {
 
         <div id="map"
             style={{ width: "80vw", height: "80vh", border: "solid black 1px", margin: "10px 10px 10px 10px" }}>
-
+ 
         </div>
 
     );
