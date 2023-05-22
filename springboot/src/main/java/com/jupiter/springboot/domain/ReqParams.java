e950f03311c688c9e619df930fa5fe1ee215bbc2
beeguriri <kkamboting@gmail.com>
2023-05-22 14:16:13 +0900

    리액트에서 받아오는 데이터 처리할 객체 만들기

---
+++ springboot/src/main/java/com/jupiter/springboot/domain/ReqParams.java
@@ -0,0 +1,25 @@
+package com.jupiter.springboot.domain;
+
+public class ReqParams {
+
+    private String year;
+    private String month;
+    private String day;
+    private String busNumber;
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getBusNumber() {
+        return busNumber;
+    }
+}
