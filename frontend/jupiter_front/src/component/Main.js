05737f7d55649d727fc10fafa8f7235e6b68ba5f
beeguriri <kkamboting@gmail.com>
2023-05-23 14:19:38 +0900

    프론트엔드 랜더링 시점 수정

--- frontend/jupiter_front/src/component/Main.js
+++ frontend/jupiter_front/src/component/Main.js
@@ -1,15 +1,69 @@
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
+
 import DrawingMap from "./DrawingMap";
 import Right from "./Right";
 
 const Main = () => {
 
+        //날짜선택 state 변수
+        const [targetDt, setTargetDt] = useState("2023-01-01");
+
+        //날짜선택박스 제어
+        const reqDate = useRef();
+    
+        // //targetDt 변경 시
+        // useEffect ( () => {
+        //     console.log('targetDt', targetDt)
+        // },[targetDt]);
+    
+        //날짜선택박스 이벤트
+        // const handleDate = () => {
+        //     setTargetDt(reqDate.current.value);
+        // };
+
+        //백엔드 응답 state 변수
+        const [data, setData] = useState();
+
+        //temp...!
+        let [temp, setTemp] = useState(0);
+    
+        // 검색버튼
+        const searchData = async (e) => {
+            e.preventDefault();
+            setTargetDt(reqDate.current.value);
+            console.log("targetDt", targetDt)
+
+            await axios
+                .post("http://localhost:8080/api/totalCount",
+    
+                    // console.log('year', targetDt.substring(2,4)),
+                    {
+                        "year": targetDt.substring(2,4),
+                        "month": targetDt.substring(5,7),
+                        "day": targetDt.substring(8,10),
+                        "busNumber": "부산70자1854",
+                    }
+                )
+                .then((response) => {
+                    console.log("response ok")
+                    console.log(response.data)
+                    setData(response.data)
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            
+            setTemp(++temp);
+        }
+
     return (
         <div className="main">
             <div className="left">
-                <DrawingMap />
+                <DrawingMap targetDt={targetDt} data={data} temp={temp}/>
             </div>
             <div className="right">
-                <Right />
+                <Right reqDate={reqDate} searchData={searchData} />
             </div>
         </div>
 
--- frontend/jupiter_front/src/component/Right.js
+++ frontend/jupiter_front/src/component/Right.js
@@ -1,59 +1,59 @@
 import axios from "axios";
 import { useState, useEffect, useRef } from "react";
 
-const Right = () => {
+const Right = (probs) => {
 
-    //날짜선택 state 변수
-    const [targetDt, setTargetDt] = useState();
+    // //날짜선택 state 변수
+    // const [targetDt, setTargetDt] = useState();
 
-    //날짜선택박스 제어
-    const reqDate = useRef();
+    // //날짜선택박스 제어
+    // const reqDate = useRef();
 
-    //targetDt 변경 시
-    useEffect ( () => {
-        console.log('targetDt', targetDt)
-    },[targetDt]);
+    // //targetDt 변경 시
+    // useEffect ( () => {
+    //     console.log('targetDt', targetDt)
+    // },[targetDt]);
 
-    //날짜선택박스 이벤트
-    const handleDate = () => {
-        setTargetDt(reqDate.current.value);
-    };
+    // //날짜선택박스 이벤트
+    // const handleDate = () => {
+    //     setTargetDt(reqDate.current.value);
+    // };
 
-    // 검색버튼
-    const searchData = async (e) => {
-        e.preventDefault();
+    // // 검색버튼
+    // const searchData = async (e) => {
+    //     e.preventDefault();
     
-        await axios
-            .post("http://localhost:8080/api/totalCount",
+    //     await axios
+    //         .post("http://localhost:8080/api/totalCount",
 
-                // console.log('year', targetDt.substring(2,4)),
-                {
-                    "year": targetDt.substring(2,4),
-                    "month": targetDt.substring(5,7),
-                    "day": targetDt.substring(8,10),
-                    "busNumber": "null",
-                }
-            )
-            .then((response) => {
-                // console.log("response ok")
-                console.log(response.data)
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
+    //             // console.log('year', targetDt.substring(2,4)),
+    //             {
+    //                 "year": targetDt.substring(2,4),
+    //                 "month": targetDt.substring(5,7),
+    //                 "day": targetDt.substring(8,10),
+    //                 "busNumber": "null",
+    //             }
+    //         )
+    //         .then((response) => {
+    //             // console.log("response ok")
+    //             console.log(response.data)
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
+//onChange={probs.handleDate} 
     return (
         <>
             <div className="selectbox">
                 <form>
-                    <input type="date" ref={reqDate} onChange={handleDate} name="reqdate" />
+                    <input type="date" ref={probs.reqDate} name="reqdate" />
                     <select>
                         <option key="banana" value="banana">전체</option>
                         <option key="apple" value="apple">사과</option>
                         <option key="orange" value="orange">오렌지</option>
                     </select>
-                    <button onClick={searchData}>조회</button>
+                    <button onClick={probs.searchData}>조회</button>
                 </form>
             </div>
             <div className="chart">
