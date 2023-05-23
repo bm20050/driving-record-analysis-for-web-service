0ebd91a644c477e82fe214aefaa932007e5ac994
Byeongmin <65452989+bm20050@users.noreply.github.com>
2023-05-23 17:57:34 +0900

    0523_csv_1차배포 (#33)

--- flask/app.py
+++ flask/app.py
@@ -1,31 +1,24 @@
 from flask import Flask, jsonify
 from http import HTTPStatus
 from flask_cors import CORS
-
+import pandas as pd
 app = Flask(__name__)
 
 CORS(app)
 
-test = [
-    {
-        'seq': -1,
-        'age': 14,
-        'userName': 'hello'
-    },
-    {
-        'seq': -1,
-        'age': 20,
-        'userName': 'hi'
-    }
-]
+def getCsv():
+    df = pd.read_csv('./data/1차배포용.csv')
+    print(df)
+    return df
 
 # @app.route('/get', methods=['GET'])
-@app.route('/get', methods=['GET'])
-def get():
-    return test
+@app.route('/prediction', methods=['GET'])
+def prediction():
+    f = getCsv().to_json()
+    return f
     # return jsonify({"test2": test2['data1']})
 # , "status": HTTPStatus.OK
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False)
 
