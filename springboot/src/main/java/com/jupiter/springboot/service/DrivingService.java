dffe0473c2dacdd13f3662ebde4a152bc374df22
beeguriri <kkamboting@gmail.com>
2023-05-24 10:06:44 +0900

    필요없는 import 구문 삭제

--- springboot/src/main/java/com/jupiter/springboot/service/DrivingService.java
+++ springboot/src/main/java/com/jupiter/springboot/service/DrivingService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.*;
 
 @Service
