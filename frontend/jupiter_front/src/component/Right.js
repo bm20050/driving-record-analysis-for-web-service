8ce2a4f342436bd067b2ec2da4133c9da22224c0
beeguriri <kkamboting@gmail.com>
2023-05-25 10:43:12 +0900

    필요없는 import문 삭제

--- frontend/jupiter_front/src/component/Right.js
+++ frontend/jupiter_front/src/component/Right.js
@@ -1,9 +1,4 @@
 import Chart from "./Chart";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
-
 
 const Right = (probs) => {
 
