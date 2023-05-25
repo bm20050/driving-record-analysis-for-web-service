8dcfea666c40c9945efec9160d20d8f7fae75483
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-05-25 12:05:46 +0900

    0525 front back (#38)
    * 위험운전 세부 분류
    
    - 백엔드 리턴 수정
    - 프론트 셀렉트 박스 선택 수정
    
    * 필요없는 import문 삭제
    
    * 필요없는 import문 삭제
    
    * 부트스트랩 적용해서 css 수정
    
    * 차트 css y축 수정

--- frontend/jupiter_front/src/component/Right.js
+++ frontend/jupiter_front/src/component/Right.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useState, useEffect, useRef } from "react";
 import Chart from "./Chart";
 
 const Right = (probs) => {
@@ -7,23 +5,31 @@ const Right = (probs) => {
     return (
         <>
             <div className="selectbox">
-                <form>
-                    <input type="date" ref={probs.reqDate} onChange={probs.handleDate} name="reqdate" />
-                    <select ref={probs.plate}>
-                        <option key="total" value="total">전체</option>
-                        <option key="부산70자1854" value="부산70자1854">부산70자1854</option>
-                        <option key="부산70자1860" value="부산70자1860">부산70자1860</option>
-                        <option key="부산70자1893" value="부산70자1893">부산70자1893</option>
-                        <option key="부산70자1894" value="부산70자1894">부산70자1894</option>
-                    </select>
-                    <select ref={probs.danger}>
-                        <option key="totalDan" value="totalDan">전체</option>
-                        <option key="suddenAcc" value="suddenAcc">급가속</option>
-                        <option key="suddenDrop" value="suddenDrop">급감속</option>
-                        <option key="suddenDeparture" value="suddenDeparture">급출발</option>
-                        <option key="suddenStop" value="suddenStop">급정지</option>
-                    </select>
-                    <button onClick={probs.searchData}>조회</button>
+                <form className="row gy-2 gx-3 align-items-center">
+                    <div className="col-auto">
+                        <input className="form-select-sm" type="date" ref={probs.reqDate} onChange={probs.handleDate} name="reqdate" style={{border: "1px lightgray solid"}}/>
+                    </div>
+                    <div className="col-auto">
+                        <select className="form-select form-select-sm" ref={probs.plate}>
+                            <option key="total" value="total">전체</option>
+                            <option key="부산70자1854" value="부산70자1854">부산70자1854</option>
+                            <option key="부산70자1860" value="부산70자1860">부산70자1860</option>
+                            <option key="부산70자1893" value="부산70자1893">부산70자1893</option>
+                            <option key="부산70자1894" value="부산70자1894">부산70자1894</option>
+                        </select>
+                    </div>
+                    <div className="col-auto">
+                        <select className="form-select form-select-sm" ref={probs.danger}>
+                            <option key="totalDan" value="totalDan">전체</option>
+                            <option key="suddenAcc" value="suddenAcc">급가속</option>
+                            <option key="suddenDrop" value="suddenDrop">급감속</option>
+                            <option key="suddenDeparture" value="suddenDeparture">급출발</option>
+                            <option key="suddenStop" value="suddenStop">급정지</option>
+                        </select>
+                    </div>
+                    <div className="col-auto">
+                        <button type="button" class="btn btn-outline-primary btn-sm" onClick={probs.searchData}>조회</button>
+                    </div>
                 </form>
             </div>
             <div className="chart">
--- springboot/src/main/java/com/jupiter/springboot/domain/ReqParams.java
+++ springboot/src/main/java/com/jupiter/springboot/domain/ReqParams.java
@@ -11,5 +11,5 @@ public class ReqParams {
     private String month;
     private String day;
     private String busNumber;
-
+    private String danger;
 }
