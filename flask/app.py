c2a36fcb0acfe55b7fc3d0de1f22739f622c7310
beeguriri <kkamboting@gmail.com>
2023-06-07 18:03:16 +0900

    파일업로드 테스트
    - 리액트에서 파일 전송
    - 스프링부트에서 받아서 파일 리스트 전송
    - 플라스크에서 리스트에 있는 파일 열어서 전처리 할 예정

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template, request, url_for
+
 from http import HTTPStatus
 from flask_cors import CORS
 import pandas as pd
@@ -6,18 +7,31 @@ app = Flask(__name__)
 
 CORS(app)
 
-def getCsv():
-    df = pd.read_csv('./data/1차배포용.csv')
-    print(df)
-    return df
+# def getCsv():
+#     df = pd.read_csv('./data/1차배포용.csv')
+#     print(df)
+#     return df
 
 # @app.route('/get', methods=['GET'])
-@app.route('/prediction', methods=['GET'])
-def prediction():
-    f = getCsv().to_json()
-    return f
-    # return jsonify({"test2": test2['data1']})
-# , "status": HTTPStatus.OK
+# @app.route('/prediction', methods=['GET'])
+# def prediction():
+#     f = getCsv().to_json()
+#     return f
+#     # return jsonify({"test2": test2['data1']})
+# # , "status": HTTPStatus.OK
+
+# 파일업로드 테스트중
+@app.route('/test', methods=['POST'])
+def test():
+    # data = request.get_json()
+    print("data", request.data)
+    print("type", type(request.data))
+    print("json", request.get_json())
+    print("json", type(request.get_json()['data']))
+    # print("len", len(request.data))
+
+    return "hello"
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=False)
