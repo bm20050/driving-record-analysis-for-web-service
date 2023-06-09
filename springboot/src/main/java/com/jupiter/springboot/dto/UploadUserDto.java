2745c9b740ac325b9fb7ea64ce7d099ca5e8ace3
beeguriri <kkamboting@gmail.com>
2023-06-09 17:59:48 +0900

    리액트에서 스프링부트로 전송 시 json parsing 때문에 어노테이션 변경

--- springboot/src/main/java/com/jupiter/springboot/dto/UploadUserDto.java
+++ springboot/src/main/java/com/jupiter/springboot/dto/UploadUserDto.java
@@ -1,8 +1,11 @@
 package com.jupiter.springboot.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@AllArgsConstructor
+@Setter @Getter
 public class UploadUserDto {
 
     private String userid;
