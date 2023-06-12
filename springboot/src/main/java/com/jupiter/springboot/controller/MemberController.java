cf7ce621556aeb81b4b005d893b3b59da4616566
beeguriri <kkamboting@gmail.com>
2023-06-12 15:49:33 +0900

    검증로직 구현

--- springboot/src/main/java/com/jupiter/springboot/controller/MemberController.java
+++ springboot/src/main/java/com/jupiter/springboot/controller/MemberController.java
@@ -5,13 +5,18 @@ import com.jupiter.springboot.dto.MemberLoginDto;
 import com.jupiter.springboot.dto.MemberLoginRespDto;
 import com.jupiter.springboot.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
@@ -19,8 +24,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/api/join")
-    public Long join (@RequestBody MemberJoinDto params){
-        return memberService.createMember(params);
+    public ResponseEntity<Object> join (@RequestBody MemberJoinDto params){
+        memberService.createMember(params);
+        return ResponseEntity.ok().body(HttpStatus.OK);
     }
 
     @PostMapping("/api/login")
