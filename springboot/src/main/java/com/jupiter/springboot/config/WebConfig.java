a9a9ef68d05488d8897f5dab83b1caebfe89ee46
beeguriri <kkamboting@gmail.com>
2023-06-12 13:02:25 +0900

    인터셉터 적용해보기

--- springboot/src/main/java/com/jupiter/springboot/config/WebConfig.java
+++ springboot/src/main/java/com/jupiter/springboot/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.jupiter.springboot.config;
 
+import com.jupiter.springboot.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +13,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**") //모든 경로에 필터 적용
+                .excludePathPatterns("/", "/api/login", "/api/logout", "/api/join",
+                        "/api/totalCount"); //필터 적용하지않을 경로
     }
 
 }
