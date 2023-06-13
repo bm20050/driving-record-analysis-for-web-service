36097dabd077651c1924039af95f68040a71b61e
beeguriri <kkamboting@gmail.com>
2023-06-13 18:04:10 +0900

    시큐리티 적용중

---
+++ springboot/src/main/java/com/jupiter/springboot/config/auth/PrincipalDetails.java
@@ -0,0 +1,64 @@
+package com.jupiter.springboot.config.auth;
+
+import com.jupiter.springboot.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Slf4j
+@RequiredArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+
+        collect.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+                return member.getRoles();
+            }
+        });
+
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        log.info("getUsername: " + member.getUsername());
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
