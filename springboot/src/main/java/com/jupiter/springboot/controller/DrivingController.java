8ba1e2220497e95a5740015b016f70cc9f951e28
beeguriri <kkamboting@gmail.com>
2023-06-13 10:51:21 +0900

    코드 정리

--- springboot/src/main/java/com/jupiter/springboot/controller/DrivingController.java
+++ springboot/src/main/java/com/jupiter/springboot/controller/DrivingController.java
@@ -19,17 +19,6 @@ public class DrivingController {
         this.drivingService = drivingService;
     }
 
-    @GetMapping("/getData")
-    public List<Driving> getList(){
-        return drivingService.getList();
-    }
-
-    @GetMapping("/api/yesterday")
-    public List<Driving> yesterday() {
-        return drivingService.yesterday();
-    }
-
-    @ResponseBody
     @PostMapping("/api/totalCount")
     public List<Driving> totalCount(@RequestBody ReqParams params) {
         System.out.println(params.toString());
