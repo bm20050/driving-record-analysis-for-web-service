7b1a2f0692662c5966d8a333bb26457d2539213b
beeguriri <kkamboting@gmail.com>
2023-06-14 14:21:42 +0900

    css 수정

--- frontend/jupiter_front/src/components/charts/Chart.js
+++ frontend/jupiter_front/src/components/charts/Chart.js
@@ -156,7 +156,7 @@ const Chart = (probs) => {
             options = {
                 chart: {
                     type: 'bar',
-                    height: '65%',
+                    height: '62%',
                     width: '100%',
                     stacked: true,
                     fontFamily: "D2Coding",
@@ -193,7 +193,7 @@ const Chart = (probs) => {
             options = {
                 chart: {
                     type: 'bar',
-                    height: '65%',
+                    height: '62%',
                     width: '100%',
                     stacked: true,
                     fontFamily: "D2Coding",
