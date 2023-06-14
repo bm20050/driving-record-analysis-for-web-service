0581feda9ae4bb30a7cf6bf73bc882cca482ffdc
beeguriri <kkamboting@gmail.com>
2023-06-14 17:56:35 +0900

    예측 모델 띄울 화면 만드는 중

--- frontend/jupiter_front/src/pages/Prediction.js
+++ frontend/jupiter_front/src/pages/Prediction.js
@@ -1,9 +1,22 @@
+import PredictMap from "../components/PredictMap";
+import data from "../data/위험운전행동_GPS.json"
+import { Button } from "react-bootstrap";
+
 const Prediction = () => {
 
+    const handlePred = () => {
+        
+    }
+
     return (
-        <>
-            예측페이지
-        </>
+        <div className="prediction">
+            <PredictMap data={data} />
+            <div className="predresult">
+                <Button onClick={handlePred}>
+                    예측결과 찍기
+                </Button>
+            </div>
+        </div>
     )
 
 }
