c9dc296cf959e0bdf49ed813fc5e204b44f508f3
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-06-16 16:11:30 +0900

    0616 prediction (#70)
    * 로그인여부에 따라 link 변경
    
    * 실시간 맵 테스트
    
    * 헤더 이미지 변경
    
    * 예측데이터 지도 그리기
    
    * 가중치 수정(테스트용)
    
    * 로그인 유무에 따른 이동 수정
    
    * css 수정

--- frontend/jupiter_front/src/components/PredictMap.js
+++ frontend/jupiter_front/src/components/PredictMap.js
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
+import tayo from '../image/tayo.png'
 
 const { kakao } = window;
 
+let map = null;
+let marker = null;
+
 const PredictMap = (probs) => {
 
-    let [map, setMap] = useState();
+    // let [map, setMap] = useState();
+
+    console.log('data', probs.data, ",", probs.x, ", ", probs.y)
 
     useEffect(() => {
 
@@ -16,27 +22,61 @@ const PredictMap = (probs) => {
 
             // 지도 그리기
             const options = {
-                center: new kakao.maps.LatLng(35.204771, 129.166241), // 지도의 중심좌표
-                level: 8 // 지도의 확대 레벨
+                center: new kakao.maps.LatLng(probs.y, probs.x), // 지도의 중심좌표
+                level: 2 // 지도의 확대 레벨
             }
 
             // 지도 생성
-            setMap(new kakao.maps.Map(container, options))
-            // console.log(map)
+            // setMap(new kakao.maps.Map(container, options))
+            map = new kakao.maps.Map(container, options)
 
         })
 
     }, []);
 
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     let imgTag = "https://github.com/beeguriri/driving-record-analysis-for-web-service/blob/main/frontend/jupiter_front/src/image/SuddenAcc.png?raw=true";
-    //     let imageSize = new kakao.maps.Size(30, 30);
+        if (!probs.data)
+            return;
 
-    //     // 마커 이미지 생성
-    //     let imgMarker = new kakao.maps.MarkerImage(imgTag, imageSize);
+        // 지도 호출
+        kakao.maps.load(() => {
 
-    // }, [])
+            // 지도를 표시할 div 
+            const container = document.getElementById('predmap');
+
+            // 지도 그리기
+            const options = {
+                center: new kakao.maps.LatLng(probs.y, probs.x), // 지도의 중심좌표
+                level: 2 // 지도의 확대 레벨
+            }
+
+            // 지도 생성
+            // setMap(new kakao.maps.Map(container, options))
+            map = new kakao.maps.Map(container, options)
+
+        })
+
+        if (marker != null)
+            marker.setMap(null);
+
+        // let imgTag = tayo
+        let imageSize = new kakao.maps.Size(30, 30);
+
+        // 마커 이미지 생성
+        let imgMarker = new kakao.maps.MarkerImage(tayo, imageSize);
+
+        // 마커 생성
+        marker = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(probs.y, probs.x),
+            image: imgMarker,
+        })
+
+        // map.center = new kakao.maps.LatLng(probs.y, probs.x);
+
+        marker.setMap(map)
+
+    }, [probs.y, probs.x])
 
 
     return (
