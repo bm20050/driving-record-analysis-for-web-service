c009c861b6255098b378018923dc02cb29a665a1
beeguriri <kkamboting@gmail.com>
2023-06-19 22:34:51 +0900

    코드정리

--- springboot/src/main/java/com/jupiter/springboot/exhandler/ErrorResult.java
+++ springboot/src/main/java/com/jupiter/springboot/exhandler/ErrorResult.java
@@ -9,19 +9,4 @@ import org.springframework.validation.BindingResult;
 public class ErrorResult {
     private String code;
     private String message;
-
-    public ErrorResult makeErrorResponse(BindingResult bindingResult){
-        String description = "";
-        String detail = "";
-        //에러가 있다면
-        if(bindingResult.hasErrors()){
-            //DTO에 설정한 meaasge값을 가져온다
-            description = bindingResult.getFieldError().getField();
-
-            //DTO에 유효성체크를 걸어놓은 어노테이션명을 가져온다.
-            detail = bindingResult.getFieldError().getCode();
-        }
-
-        return new ErrorResult(description, detail);
-    }
 }
