13a2f2e8ad69981317987c326fc3ae66b60caa41
beeguriri <kkamboting@gmail.com>
2023-06-19 17:07:36 +0900

    log 추가

--- springboot/src/main/java/com/jupiter/springboot/interceptor/LoginCheckInterceptor.java
+++ springboot/src/main/java/com/jupiter/springboot/interceptor/LoginCheckInterceptor.java
@@ -21,6 +21,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
         if(session == null || session.getAttribute("loginMember")==null) {
             log.info("미인증 사용자 요청");
+            log.info("{}", handler);
 
             throw new RuntimeException("사용자 인증정보 없음");
         }
