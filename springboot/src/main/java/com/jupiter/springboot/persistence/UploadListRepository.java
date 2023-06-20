818bb3b6ef28d2053a8bb76d11ec218a6bfcc4eb
beeguriri <kkamboting@gmail.com>
2023-06-20 12:13:58 +0900

    지연로딩 문제 확인

--- springboot/src/main/java/com/jupiter/springboot/persistence/UploadListRepository.java
+++ springboot/src/main/java/com/jupiter/springboot/persistence/UploadListRepository.java
@@ -1,9 +1,15 @@
 package com.jupiter.springboot.persistence;
 
+import com.jupiter.springboot.domain.Member;
 import com.jupiter.springboot.domain.UploadList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UploadListRepository extends JpaRepository<UploadList, Long> {
 
+    @Query("select u, m from UploadList u join fetch u.member m where m.id = :memberId")
+    List<UploadList> findWithMember(Long memberId);
 
 }
