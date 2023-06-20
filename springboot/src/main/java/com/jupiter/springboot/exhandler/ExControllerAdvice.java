2240e19701626026a047d44bdece7d610fd73a7f
beeguriri <kkamboting@gmail.com>
2023-06-20 12:13:20 +0900

    회원가입 검증

--- springboot/src/main/java/com/jupiter/springboot/exhandler/ExControllerAdvice.java
+++ springboot/src/main/java/com/jupiter/springboot/exhandler/ExControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.jupiter.springboot.exhandler;
 
+import com.jupiter.springboot.exception.BlankException;
 import com.jupiter.springboot.exception.DupUserException;
 import com.jupiter.springboot.exception.NoUserException;
 import com.jupiter.springboot.exception.WrongPasswordException;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -42,10 +44,17 @@ public class ExControllerAdvice {
         return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResult> methodValidException(MethodArgumentNotValidException e, HttpServletRequest request){
-        log.error("[exceptionHandler] ex [getCode={}][getField={}][getDefaultMessage={}]", e.getFieldError().getCode(), e.getFieldError().getField(), e.getFieldError().getDefaultMessage());
-        ErrorResult errorResult = new ErrorResult(e.getFieldError().getCode(), e.getFieldError().getDefaultMessage());
-        return new ResponseEntity<ErrorResult>(errorResult, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResult> validException(ConstraintViolationException e){
+        log.error("[exceptionHandler] ex [getMessage={}]", e.getMessage());
+        ErrorResult errorResult = new ErrorResult("field", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
+
+//    @ExceptionHandler
+//    public ResponseEntity<ErrorResult> blankExHandler(BlankException e) {
+//        log.error("[exceptionHandler] ex", e);
+//        ErrorResult errorResult = new ErrorResult(e.getMessage(), "notEmpty");
+//        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+//    }
 }
