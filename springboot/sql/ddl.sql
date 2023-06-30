588e91fbca74bb1bfa12eff7f18b94682aad78a7
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-06-30 10:10:53 +0900

    0630 마무리 (#81)
    * 스프링부트 빌드
    
    * sql 수정 등 코드정리
    
    * 파이썬 필요패키지 업데이트
    
    * 팀 문서 업데이트

--- springboot/sql/ddl.sql
+++ springboot/sql/ddl.sql
@@ -6,7 +6,7 @@ CREATE TABLE `driving` (
            `일` VARCHAR(2) NULL,
            `시` VARCHAR(2) NULL,
            `시분초` VARCHAR(10) NULL,
-           `속도` INT NULL,
+           `차량속도` INT NULL,
            `가속도` INT NULL,
            `RPM` INT NULL,
            `급가속` INT DEFAULT 0,
@@ -18,20 +18,6 @@ CREATE TABLE `driving` (
            PRIMARY KEY (`id`)
 );
 
-
-CREATE TABLE `reqlog` (
-          `seq` INT NOT NULL AUTO_INCREMENT,
-          `요청일시` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          `차량번호` VARCHAR(45) NULL,
-          `운행일시` VARCHAR(45) NULL,
-          `운행회차` VARCHAR(45) NULL,
-          `요청메뉴` VARCHAR(45) NULL,
-          `응답메시지` VARCHAR(45) NULL,
-          PRIMARY KEY (`seq`)
-);
-
-Hibernate:
-
 create table member (
                         member_id bigint not null auto_increment,
                         email varchar(255),
@@ -39,9 +25,10 @@ create table member (
                         roles varchar(255),
                         userid varchar(255),
                         username varchar(255),
+                        created_at datetime(6),
+                        modified_at datetime(6),
                         primary key (member_id)
-) engine=InnoDB
-Hibernate:
+)
 
 create table my_file_list (
                               file_id bigint not null auto_increment,
@@ -50,33 +37,15 @@ create table my_file_list (
                               new_name varchar(255),
                               origin_name varchar(255),
                               upload_id bigint,
+                              created_at datetime(6),
+                              modified_at datetime(6),
                               primary key (file_id)
-) engine=InnoDB
-Hibernate:
+)
 
 create table upload_list (
                              upload_id bigint not null auto_increment,
-                             request_date datetime(6),
                              member_id bigint,
+                             created_at datetime(6),
+                             modified_at datetime(6),
                              primary key (upload_id)
-) engine=InnoDB
-Hibernate:
-
-alter table member
-drop index UK_6yhxjegychh1rq9jfynisnhro
-    Hibernate:
-
-alter table member
-    add constraint UK_6yhxjegychh1rq9jfynisnhro unique (userid)
-    Hibernate:
-
-alter table my_file_list
-    add constraint FK8kedpihxotws42foqryh0hqrk
-        foreign key (upload_id)
-            references upload_list (upload_id)
-    Hibernate:
-
-alter table upload_list
-    add constraint FKger9h8khg4egenfr4v93wohxn
-        foreign key (member_id)
-            references member (member_id)
+)
--- springboot/src/main/java/com/jupiter/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/jupiter/springboot/SpringbootApplication.java
@@ -3,12 +3,7 @@ package com.jupiter.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
